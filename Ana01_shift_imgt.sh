9ba44d1c6d25ba669de43273c627c6ab8ae2cc62
Li-Ming Hsu <50681765+tw70455@users.noreply.github.com>
2025-09-23 17:00:23 -0400

    Update Ana01_shift_imgt.sh

--- Ana01_shift_imgt.sh
+++ Ana01_shift_imgt.sh
@@ -9,6 +9,8 @@ foreach ratNum ($NUMS)
 	if  ( -e "${FILE_PATH}${ratNum}_Anat.nii.gz" ) then
 		# Remove the previous file that has been resampled to 0.32 mm resolution if it exists
 		rm "${FILE_PATH}${ratNum}_Anat_032.nii.gz"
+		3drefit -orient RPS "${FILE_PATH}${ratNum}_Anat.nii.gz"
+		3drefit -orient RPS "${FILE_PATH}${ratNum}_Func.nii.gz"
 		3dresample -dxyz 0.32 0.32 0.32 -inset "${FILE_PATH}${ratNum}_Anat.nii.gz" -prefix "${FILE_PATH}${ratNum}_Anat_032.nii.gz"
 	else
 		echo "Warning! ${FILE_PATH}${ratNum}_Anat.nii.gz not found."
