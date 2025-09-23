b8c801307de683133854773c2312381f95e4b8ea
Li-Ming Hsu <50681765+tw70455@users.noreply.github.com>
2025-09-23 17:06:45 -0400

    Update Ana04_shift2Temp_ANTs.sh

--- Ana04_shift2Temp_ANTs.sh
+++ Ana04_shift2Temp_ANTs.sh
@@ -8,36 +8,78 @@ set NUMS="Subj01 Subj02 Subj03"
 # Set base directory (where the script is located)
 set BASE_DIR = `dirname $0`
 
+# Template related files
+set TEMPLATE_IMG  = "${BASE_DIR}/Template/ZTE_Template_mean_Ants_resample_allineate_resample.nii.gz"
+set TEMPLATE_REF  = "${BASE_DIR}/Template/ZTE_Template_mean_Ants_resample_allineate_2ZTE.nii.gz" 
+set TEMPLATE_MASK = "${BASE_DIR}/Template/ZTE_Template_mean_Ants_resample_allineate_resample_mask.nii.gz"                           # 
+
 
 foreach ratNum(${NUMS}) # 
 	if  ( -e "${FILE_PATH}${ratNum}_Func.nii.gz" ) then
 
-		# Remove previous masked anatomical file if it exists
-		rm "${FILE_PATH}${ratNum}_Anat_masked.nii.gz"
-		# Create a masked anatomical image using the sum of mask and add file
-		3dcalc -a "${FILE_PATH}${ratNum}_Anat.nii.gz" -b "${FILE_PATH}${ratNum}_Anat_mask.nii.gz" -c "${FILE_PATH}${ratNum}_Anat_032_add.nii" -expr 'a*ispositive(b+c)' -prefix "${FILE_PATH}${ratNum}_Anat_masked.nii.gz"
-		3dcalc -a "${FILE_PATH}${ratNum}_Anat.nii.gz" -b "${FILE_PATH}${ratNum}_Anat_mask.nii.gz" -expr 'a*ispositive(b)' -prefix "${FILE_PATH}${ratNum}_Anat_masked.nii.gz"
 
 
-		# Remove previous bias-corrected anatomical file if it exists
-		rm "${FILE_PATH}${ratNum}_Anat_masked_unify5.nii.gz"
-		# Apply bias field correction (unifize) with radius 5
-		3dUnifize -Urad 5 -prefix "${FILE_PATH}${ratNum}_Anat_masked_unify5.nii.gz" "${FILE_PATH}${ratNum}_Anat_masked.nii.gz"
+		# create masked anatomical image
+		rm -f "${FILE_PATH}${ratNum}_Anat_masked.nii.gz"
+	    3dcalc \
+	      -a "${FILE_PATH}${ratNum}_Anat.nii.gz" \
+	      -b "${FILE_PATH}${ratNum}_Anat_mask.nii.gz" \
+	      -expr 'a*ispositive(b)' \
+	      -prefix "${FILE_PATH}${ratNum}_Anat_masked.nii.gz"
+
+
+		# bias correction
+		rm -f "${FILE_PATH}${ratNum}_Anat_masked_unify5.nii.gz"
+		3dUnifize -Urad 5 \
+		  -prefix "${FILE_PATH}${ratNum}_Anat_masked_unify5.nii.gz" \
+		  "${FILE_PATH}${ratNum}_Anat_masked.nii.gz"
+
+
+
+	    # initial normalization
+	    rm -f "${FILE_PATH}${ratNum}_Anat_affineOnly_*"
+	    antsRegistrationSyN.sh \
+	      -d 3 \
+	      -f "${TEMPLATE_IMG}" \
+	      -m "${FILE_PATH}${ratNum}_Anat_masked_unify5.nii.gz" \
+	      -o "${FILE_PATH}${ratNum}_Anat_affineOnly_" \
+	      -t a \
+	      -n ${OMP_NUM_THREADS}
+		  
+		  
+		# inverse template mask to native space
+		rm -f "${FILE_PATH}${ratNum}_TemplateMask_inSubj_AffineOnly.nii.gz"
+	    antsApplyTransforms \
+	      -d 3 --float 1 \
+	      -i "${TEMPLATE_MASK}" \
+	      -r "${FILE_PATH}${ratNum}_Anat_masked_unify5.nii.gz" \
+	      -o "${FILE_PATH}${ratNum}_TemplateMask_inSubj_AffineOnly.nii.gz" \
+	      -t "[${FILE_PATH}${ratNum}_Anat_affineOnly_0GenericAffine.mat,1]" \
+	      -n NearestNeighbor
+
+
+
+		# remask the anatomical image
+		rm -f "${FILE_PATH}${ratNum}_Anat_Masked.nii.gz"
+	    3dcalc \
+	      -a "${FILE_PATH}${ratNum}_Anat.nii.gz" \
+	      -b "${FILE_PATH}${ratNum}_TemplateMask_inSubj_AffineOnly.nii.gz" \
+	      -expr 'a*step(b)' \
+	      -prefix "${FILE_PATH}${ratNum}_Anat_Masked.nii.gz"
+
+		# bias-correction
+		rm -f "${FILE_PATH}${ratNum}_Anat_Masked_unify5.nii.gz"
+	    3dUnifize -Urad 5 \
+	      -prefix "${FILE_PATH}${ratNum}_Anat_masked_unify5.nii.gz" \
+	      "${FILE_PATH}${ratNum}_Anat_Masked.nii.gz"
+
+
 		
-		
-		
-		3drefit -orient RPS "${FILE_PATH}${ratNum}_Anat_masked_unify5.nii.gz"
-		
-		# Remove previous ANTs registration files if they exist
-		rm "${FILE_PATH}${ratNum}_Anat_masked_unify5_Ants*"
-		# Register anatomical to template using ANTs (SyN registration)
+		# Register anatomical to template using ANTs
 		antsRegistrationSyN.sh -d 3 -f "${BASE_DIR}/Template/ZTE_Template_mean_Ants_resample_allineate_resample.nii.gz" \
-			-m "${FILE_PATH}${ratNum}_Anat_masked_unify5.nii.gz" \
-				-o "${FILE_PATH}${ratNum}_Anat_masked_unify5_Ants" -t s -n 4
+		-m "${FILE_PATH}${ratNum}_Anat_masked_unify5.nii.gz" \
+			-o "${FILE_PATH}${ratNum}_Anat_masked_unify5_Ants" -t s -n 4
 
-
-
-		3drefit -orient RPS "${FILE_PATH}${ratNum}_Func.nii.gz"
 
 		# Remove previous warped functional file if it exists
 		rm "${FILE_PATH}${ratNum}_Func_ANTsWarped.nii.gz"
@@ -50,6 +92,7 @@ foreach ratNum(${NUMS}) #
 		  -t "${FILE_PATH}${ratNum}_Anat_masked_unify5_Ants0GenericAffine.mat" \
 		  -n Linear
 
+		
 
 	else
 		echo "Warning! ${FILE_PATH}${ratNum}_Func.nii.gz not found."
